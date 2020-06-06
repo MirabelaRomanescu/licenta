e9600a161e5dec718fbddb023a6dfb2c460e69e5
MirabelaRomanescu <mirabela.romanescu@gmail.com>
2020-06-06 18:44:43 +0300

    fixed one bug

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(<App />,
   document.getElementById('root')
